21c476c32f4f0ceda80491684ee3277f31d91f6c
AlexAlt <alexandra.m.altieri@gmail.com>
2015-08-21 11:57:25 -0700

    Add spec and method to update the name of the stylist

--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,1 +1,36 @@
 require('spec_helper')
+
+describe(Stylist) do 
+
+  describe('.all') do
+    it('initially returns and empty array') do
+      expect(Stylist.all()).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it('saves a stylist to the database') do
+      stylist = Stylist.new({:name => "River", :id => nil})
+      stylist.save()
+      expect(Stylist.all()).to(eq([stylist]))
+    end
+  end
+
+  describe('#==') do
+    it('is the same stylist if the name and id are the same') do
+      stylist = Stylist.new({:name => "River", :id => nil})
+      stylist2 = Stylist.new({:name => "River", :id => nil})
+      expect(stylist).to(eq(stylist2))
+    end
+  end
+
+  describe('#update') do
+    it('updates the name of the stylist') do
+      stylist = Stylist.new({:name => "River", :id => nil})
+      stylist.save()
+      stylist.update({:name => "River Tam"})
+      expect(stylist.name()).to(eq("River Tam"))
+    end
+  end
+
+end 
